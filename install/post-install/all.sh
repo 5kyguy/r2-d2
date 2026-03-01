87e36d700957f805319378ef9c5ed223aed6b4de
5kyguy <0x5kyguy@gmail.com>
2026-03-01 22:29:21 +0530

    feat: add migrations for future updates compatibility

--- install/post-install/all.sh
+++ install/post-install/all.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 run_logged $OMARCHY_INSTALL/post-install/pacman.sh
+run_logged omarchy-migrate
 source $OMARCHY_INSTALL/post-install/allow-reboot.sh
 source $OMARCHY_INSTALL/post-install/finished.sh
--- install/preflight/migrations.sh
+++ install/preflight/migrations.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
+# Prepare migration state directory. Migrations are run at end of install (omarchy-migrate).
 OMARCHY_MIGRATIONS_STATE_PATH=~/.local/state/omarchy/migrations
 mkdir -p $OMARCHY_MIGRATIONS_STATE_PATH
-
-for file in ~/.local/share/omarchy/migrations/*.sh; do
-  touch "$OMARCHY_MIGRATIONS_STATE_PATH/$(basename "$file")"
-done
