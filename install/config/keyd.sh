dfa73c4d4c69c5190a230905725044b4a7505dea
5kyguy <0x5kyguy@gmail.com>
2026-06-25 07:26:42 +0530

    fix: add missing keyd install

--- install/config/keyd.sh
+++ install/config/keyd.sh
@@ -4,6 +4,8 @@
 
 R2D2_PATH="${R2D2_PATH:-$HOME/.local/share/r2-d2}"
 
+r2-d2-pkg-add keyd
+
 sudo mkdir -p /etc/keyd
 sudo cp "$R2D2_PATH/default/keyd/default.conf" /etc/keyd/default.conf
 sudo systemctl enable --now keyd
